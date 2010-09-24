9d9958bdcaf72df73fe30b406d502d61bb59392c
Shabda Raaj <shabda@agiliq.com>
2010-09-24 14:35:16 +0530

    Added example

--- books/mylibrary/views.py
+++ books/mylibrary/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from mylibrary.grids import SimpleGrid
+from mylibrary.models import Book
+
+def simple(request):
+    books = Book.objects.all()
+    return SimpleGrid(request, books).render_to_response("mylibrary/simple.html")
