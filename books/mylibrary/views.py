2953736714b383e4a02f1a3c4c6fd20f3c48c4fb
Shabda Raaj <shabda@agiliq.com>
2010-09-24 14:51:30 +0530

    Added and Documented a real example

--- books/mylibrary/views.py
+++ books/mylibrary/views.py
@@ -1,6 +1,11 @@
-from mylibrary.grids import SimpleGrid
+from mylibrary.grids import SimpleGrid, RealGrid
 from mylibrary.models import Book
 
 def simple(request):
     books = Book.objects.all()
     return SimpleGrid(request, books).render_to_response("mylibrary/simple.html")
+
+
+def real(request):
+    books = Book.objects.all()
+    return RealGrid(request, books).render_to_response("mylibrary/real.html")
