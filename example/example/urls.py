c84467c0f29303b3823fa309a8f333a4bbff7874
shivakrishnab <shiva@agiliq.com>
2013-09-27 14:38:10 +0530

    Added a new example project app

---
+++ example/example/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'example.views.home', name='home'),
+    # url(r'^example/', include('example.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('blog_grids.views',
+    url(r'^$', 'blog_grid', name='blog_grids_blog_grid'),
+)
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
