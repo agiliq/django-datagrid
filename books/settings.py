02d2c910c77cb4c11fdbba221c9d01231688461a
shivakrishnab <shiva@agiliq.com>
2013-10-10 12:29:08 +0530

    Changed database name to app name

--- books/settings.py
+++ books/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pollango.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'books.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
